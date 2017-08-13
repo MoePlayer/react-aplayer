7da28710830c37067eaeff5c924dbdb3f4f72863
Sabrina Luo <luohaojing@gmail.com>
2017-08-13 22:48:30 +0800

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/react-aplayer.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'react-aplayer.min.js',
+    libraryTarget: 'umd'
+  },
+  externals: ['react'],
+  module: {
+    rules: [
+      {
+        test: /(\.js)|(\.jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        comparisons: false,
+      },
+      output: {
+        comments: false,
+      },
+      sourceMap: true,
+    })]
+};
