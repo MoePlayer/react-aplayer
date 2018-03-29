5fe4310e1f9a08271317c4c4b9be1ab4088abffe
Sabrina Luo <luohaojing@gmail.com>
2018-03-30 02:42:31 +0800

    update demo page, add logo

--- src/app.js
+++ src/app.js
@@ -18,10 +18,22 @@ class App extends PureComponent {
     this.ap1 = ap;
   };
 
+  renderReactLogo = () => {
+    return (
+      <img
+        className="react-logo"
+        src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
+      />
+    );
+  };
+
   render() {
     return (
       <div className="landing">
         <h1 className="main-title">React Aplayer</h1>
+        <h2>
+          {this.renderReactLogo()} x <span className="icon-lollipop">🍭</span>
+        </h2>
         <h3>🍭 Wow, A react wrapper of the beautiful HTML5 music Aplayer</h3>
 
         <div className="aplayer-wrap">
